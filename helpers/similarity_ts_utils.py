84eccc66deb37218f5ab08ca0f415c43e02d0c82
Felipe Escalera González <felescgon@alum.us.es>
2023-08-06 02:48:42 +0200

    Issue #13 solved: Must use [] in XConfig classes if args are optional

--- helpers/similarity_ts_utils.py
+++ helpers/similarity_ts_utils.py
@@ -5,12 +5,11 @@ from similarity_ts.plots.plot_config import PlotConfig
 
 def create_similarity_ts_config(arguments, ts2_names, header_names):
     metric_config = None
-    plot_config = None if arguments.timestamp_frequency_seconds is None else PlotConfig(arguments.figures,
-                                                                                        arguments.timestamp_frequency_seconds)
+    plot_config = None
     if arguments.metrics is not None or arguments.figures is not None:
         metric_config = MetricConfig(arguments.metrics) if arguments.metrics else MetricConfig(None)
         plot_config = PlotConfig(arguments.figures,
-                                 arguments.timestamp_frequency_seconds) if arguments.figures else PlotConfig(None,
+                                 arguments.timestamp_frequency_seconds) if arguments.figures else PlotConfig([],
                                                                                                              arguments.timestamp_frequency_seconds)
     return SimilarityTsConfig(metric_config, plot_config, arguments.stride, arguments.window_selection_metric,
                               ts2_names,
